9b53f05e78b549827a5f0516b2bd59028991a9e6
meagbenyegah <meagbenyegah@gmail.com>
2023-11-01 22:02:52 +0000

    Login screen update

--- lib/login.dart
+++ lib/login.dart
@@ -50,6 +50,65 @@ class _LoginPageState extends State<LoginPage> {
                 ],
               ),
             ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                          child:
+                          Padding(
+                              padding: const EdgeInsets.fromLTRB(20, 20, 20, 20),
+                              child:
+                              ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                    foregroundColor: Colors.white,
+                                    backgroundColor: Colors.black87,
+                                    padding: const EdgeInsets.fromLTRB(15, 15, 15, 15),
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(7),
+                                    ),
+                                    textStyle: const TextStyle(fontSize: 16)),
+                                child: const Text('Log in'),
+                                onPressed: () {
+
+                                },
+                              ))),
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                          child:
+                          Padding(
+                              padding: const EdgeInsets.fromLTRB(20, 0, 20, 50),
+                              child:
+                              ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                    foregroundColor: Colors.black,
+                                    backgroundColor: Colors.white60,
+                                    padding: const EdgeInsets.fromLTRB(15, 15, 15, 15),
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(7),
+                                    ),
+                                    textStyle: const TextStyle(fontSize: 16)),
+                                child: const Text('Create Account'),
+                                onPressed: () {
+
+                                },
+                              ))),
+                    ],
+                  ),
+                  const Text(
+                      "or continue with",
+                      style: TextStyle(color: Colors.black, fontSize: 16.0)
+                  ),
+                  const SizedBox(height: 200)
+                ],
+              ),
+            )
           ],
         ),
       ),
